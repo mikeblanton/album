1a93dd5c025619f216df1388262b972a9973e9dc
Michael Blanton <mblanton@gmail.com>
2017-09-16 17:48:38 -0700

    Updated AlbumList code to pull remote data

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!!!</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+
+  // Initialize the state with an empty albums list
+  state = { albums: [] };
+
+  // Called when the Component first loads
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  // Helper function to render albums
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <Text key={album.title}>{album.title}</Text>
+    );
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
